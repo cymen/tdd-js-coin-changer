5b9da0a248b07b04aad4d8f55f7bc305136746a3
Cymen Vig <cymenvig@gmail.com>
2014-03-13 15:58:19 -0700

    Minor tweaks

--- code_spec.js
+++ code_spec.js
@@ -1,12 +1,16 @@
 describe('Coin changer', function() {
+  var bank;
+  beforeEach(function() {
+    bank = {
+      quarters: 0,
+      dimes: 0,
+      nickles: 0,
+      pennies: 0
+    };
+  });
+
   it('gives no change (no coins) for amount of 0 cents', function() {
-    var bank = {
-        quarters: 0,
-        dimes: 0,
-        nickles: 0,
-        pennies: 0
-      },
-      changer = new CoinChanger(bank);
+    var changer = new CoinChanger(bank);
 
     expect(changer.makeChange(0)).toEqual({
       quarters: 0,
@@ -17,13 +21,8 @@ describe('Coin changer', function() {
   });
 
   it('gives a single penny change for amount of 1 cents', function() {
-    var bank = {
-        quarters: 0,
-        dimes: 0,
-        nickles: 0,
-        pennies: 20
-      },
-      changer = new CoinChanger(bank);
+    bank.pennies = 20;
+    var changer = new CoinChanger(bank);
 
     expect(changer.makeChange(1)).toEqual({
       quarters: 0,
@@ -34,13 +33,9 @@ describe('Coin changer', function() {
   });
 
   it('gives a penny and a nickle for amount of 6 cents', function() {
-    var bank = {
-        quarters: 0,
-        dimes: 0,
-        nickles: 5,
-        pennies: 20
-      },
-      changer = new CoinChanger(bank);
+    bank.pennies = 20;
+    bank.nickles = 5;
+    var changer = new CoinChanger(bank);
 
     expect(changer.makeChange(6)).toEqual({
       quarters: 0,
